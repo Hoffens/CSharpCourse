0b310c2e6db52b663a2700f4cf56545666fc9a61
Nicolas Fernandez <nicolas.fernandez.reinoso@gmail.com>
2023-05-17 22:16:18 -0400

    Add delegates

--- 05 - Functions/Delegates.cs
+++ 05 - Functions/Delegates.cs
@@ -4,6 +4,8 @@
     {
         public Delegates() 
         {
+            // Delegates allow us to save references of functions
+
             /* 
              * Action
              * ONLY for void functions
@@ -28,11 +30,31 @@
              * Func
              * For functions that return a value
              */
-            Func<string> myNewFunction3;   // Stores a reference to a string function without parameters
+            Func<string> myNewFunction3;   // Stores a reference to a function without parameters that returns a string
             myNewFunction3 = CurrentDate;
             string actualDate = myNewFunction3();   // Invokes the function, now actualDate has a value.
 
-            /* TODO */
+            Func<int, string> myNewFunction4;   // the last data type is the type that returns the function
+            myNewFunction4 = ReceiveIntReturnString;
+            string newValue = myNewFunction4(23);   // Invokes the function, receive an int and returns a string
+
+            /*
+             * Predicate
+             * It's a Func that always return a bool
+            */
+            Predicate<int> myPredicate;     // Stores a reference to a function with an int parameter that returns a bool
+            myPredicate = IsEven;
+            bool isEven = myPredicate(2);
+
+            // Anonymous function: Functions without name
+            Action anonFunc = () =>
+            {
+                Console.WriteLine("Anon. Func!");
+            };
+            anonFunc(); // executes the anon func
+
+            Func<int, int> anonFunc2 = a => 2 * a;
+            int result = anonFunc2(5);  // returns 2 * a
         }
 
         void MyFunctionWithoutParams()
@@ -50,7 +72,7 @@
             Console.WriteLine($"{name}");
         }
 
-        static void MyFullName(string name, string lastname)
+        static void MyFullName(string name, string lastname = "noLastName")
         {
             Console.WriteLine($"{name} {lastname}");
         }
@@ -58,6 +80,16 @@
         static string CurrentDate()
         {
             return DateTime.Now.ToString();
+        }
+        
+        static string ReceiveIntReturnString(int value)
+        {
+            return value.ToString();
+        }
+
+        bool IsEven(int value)
+        {
+            return value % 2 == 0;
         }
     }
 }
